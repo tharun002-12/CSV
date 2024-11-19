81705a9ae0c92bab1c864ad58ffeaed48c2d871b
tharun002-12 <tharund002@gmail.com>
2024-11-20 02:34:33 +0530

    Update prop.csv.py

--- CSV 2 Data Science/prop.csv.py
+++ CSV 2 Data Science/prop.csv.py
@@ -7,34 +7,34 @@ def sigmoid(x):
     return 1/(1 + np.exp(-x))
 def derivatives_sigmoid(x):
     return x * (1 - x)
-# Variable initialization
+
 epoch = 5000
 lr = 0.1
-inputlayer_neurons = 2 # number of features in the dataset
-hiddenlayer_neurons = 3 # number of hidden layer neurons
-output_neurons = 1 # number of neurons at output layer
-# weight and bias initialization
+inputlayer_neurons = 2 
+hiddenlayer_neurons = 3 
+output_neurons = 1 
+
 wh = np.random.uniform(size=(inputlayer_neurons, hiddenlayer_neurons))
 bh = np.random.uniform(size=(1, hiddenlayer_neurons))
 wout = np.random.uniform(size=(hiddenlayer_neurons, output_neurons))
 bout = np.random.uniform(size=(1, output_neurons))
-# training the model
+
 for i in range(epoch):
-    # Forward Propagation
+    
     hinp1 = np.dot(X, wh)
     hinp = hinp1 + bh
     hlayer_act = sigmoid(hinp)
     outinp1 = np.dot(hlayer_act, wout)
     outinp = outinp1 + bout
     output = sigmoid(outinp)
-    # Backpropagation
+    
     EO = y - output
     outgrad = derivatives_sigmoid(output)
     d_output = EO * outgrad
     EH = d_output.dot(wout.T)
     hiddengrad = derivatives_sigmoid(hlayer_act)
     d_hiddenlayer = EH * hiddengrad
-    # Updating the weights and biases
+    
     wout += hlayer_act.T.dot(d_output) * lr
     wh += X.T.dot(d_hiddenlayer) * lr
 print("Input: \n" + str(X))
