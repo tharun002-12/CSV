509d5b45829ba88555da1916fd367fdb93217ecb
tharun002-12 <tharund002@gmail.com>
2024-11-20 02:32:48 +0530

    Update medico.csv.py

--- CSV 2 Data Science/medico.csv.py
+++ CSV 2 Data Science/medico.csv.py
@@ -9,15 +9,13 @@ names = ['age', 'sex', 'cp', 'trestbps', 'chol', 'fbs', 'restecg', 'thalach', 'e
 'thal', 'heartdisease'] 
 heartDisease = pd.read_csv(Cleveland_data_URL, names=names)
 heartDisease = heartDisease.replace('?', np.nan) 
-# Display the data 
+
 print("Few examples from the dataset are given below") 
 print(heartDisease.head()) 
 model = BayesianModel([('age', 'trestbps'), ('age', 'fbs'), ('sex', 'trestbps'), ('sex', 'trestbps'), 
 ('exang', 'trestbps'), ('trestbps', 'heartdisease'), ('fbs', 'heartdisease'), ('heartdisease', 'restecg'), 
 ('heartdisease', 'thalach'), ('heartdisease', 'chol')]) 
-# Learning CPDs using Maximum Likelihood Estimators 
 model.fit(heartDisease, estimator=MaximumLikelihoodEstimator) 
 HeartDisease_infer = VariableElimination(model) 
-# Computing the probability of heartdisease given age=28 
 q = HeartDisease_infer.query(variables=['heartdisease'], evidence={'age': 28}) 
 print(q['heartdisease']) 
